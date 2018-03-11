09b8c065a99d348f9201b5d3e8ada6d2a5004609
VuongLong2196 <37734916+VuongLong2196@users.noreply.github.com>
2018-03-11 21:32:40 +0700

    kệ sách

---
+++ KeSach.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Library1
+{
+    public partial class KeSach : Form
+    {
+        public KeSach()
+        {
+            InitializeComponent();
+        }
+        string strConn = @"Data Source=CUONG;Initial Catalog=QuanLyThuVien;Integrated Security=True";
+        SqlConnection conn;
+        private void LoadData()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM KeSach", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("SuaKeSach", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter p = new SqlParameter("@Ma", MaKS.Text);
+            cmd.Parameters.Add(p);
+
+            p = new SqlParameter("@Ten", TenKS.Text);
+            cmd.Parameters.Add(p);
+
+           
+
+            p = new SqlParameter("@ViTri",ViTri.Text);
+            cmd.Parameters.Add(p);
+
+
+            int count = cmd.ExecuteNonQuery();
+
+            if (count > 0)
+            {
+                MessageBox.Show("Sửa thành công!");
+                LoadData();
+            }
+            else MessageBox.Show("Không sửa được!");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Search_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muôn xóa bản ghi đang chọn không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                SqlCommand cmd = new SqlCommand("XoaKeSach", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+
+                SqlParameter p = new SqlParameter("@Ma", MaKS.Text);
+                cmd.Parameters.Add(p);
+
+                int count = cmd.ExecuteNonQuery();
+
+                if (count > 0)
+                {
+                    MessageBox.Show("Xóa thành công!");
+                    LoadData();
+                }
+                else MessageBox.Show("Không thể xóa bản ghi hiện thời!");
+            }
+        }
+
+        private void dataGridView1_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            dataGridView1.Rows[e.RowIndex].Cells["clSTT"].Value = e.RowIndex + 1;
+        }
+
+        private void buttonADD_Click(object sender, EventArgs e)
+        {
+            //sinh ma tu tang
+            int count1 = 0;
+            count1 = dataGridView1.Rows.Count;//dem cac dong trong datagridview roi gan cho count
+            string c1 = "";
+            int c2 = 0;
+            c1 = Convert.ToString(dataGridView1.Rows[count1 - 2].Cells[1].Value);
+
+            c2 = Convert.ToInt32(c1.Remove(0, 4));
+            if (c2 + 1 < 10)
+            {
+                MaKS.Text = "KS110" + (c2 + 1).ToString();
+            }
+            else if (c2 + 1 < 100)
+            {
+                MaKS.Text = "KS11" + (c2 + 1).ToString();
+
+            }
+
+            SqlCommand cmd = new SqlCommand("ThemKeSach", conn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter p = new SqlParameter("@Ma", MaKS.Text);
+            cmd.Parameters.Add(p);
+
+          
+
+            p = new SqlParameter("@Ten", TenKS.Text);
+            cmd.Parameters.Add(p);
+
+            p = new SqlParameter("@ViTri", ViTri.Text);
+            cmd.Parameters.Add(p);
+
+            
+            // Thực thi thủ tục
+            int count = cmd.ExecuteNonQuery();
+            if (count > 0)
+            {
+                MessageBox.Show("Thêm mới thành công");
+                LoadData();
+            }
+        }
+
+        private void buttonThem_Click(object sender, EventArgs e)
+        {
+            MaKS.Enabled = false;
+            TenKS.Text = "";
+            buttonADD.Enabled = true;
+
+            ViTri.Text = "";
+           
+            MaKS.Focus();
+        }
+
+        private void TimKiem_TextChanged(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(strConn);
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from KeSach where TenKS like '" + "%" + TimKiem.Text + "%'", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void KeSach_Load_1(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(strConn);
+            conn.Open();
+            LoadData();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                MaKS.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["clMa"].Value);
+                TenKS.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["clTen"].Value);
+                ViTri.Text = Convert.ToString(dataGridView1.CurrentRow.Cells["clVT"].Value);
+
+
+            }
+        }
+    }
+}
---
+++ KeSach.designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Library1
+{
+    partial class KeSach
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clSTT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clMa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clVT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonThem = new System.Windows.Forms.Button();
+            this.buttonADD = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonXoa = new System.Windows.Forms.Button();
+            this.Ten = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TimKiem = new System.Windows.Forms.TextBox();
+            this.ViTri = new System.Windows.Forms.TextBox();
+            this.TenKS = new System.Windows.Forms.TextBox();
+            this.MaKS = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(525, 255);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clSTT,
+            this.clMa,
+            this.clTen,
+            this.clVT});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 257);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(525, 150);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.dataGridView1_RowPrePaint);
+            // 
+            // clSTT
+            // 
+            this.clSTT.HeaderText = "STT";
+            this.clSTT.Name = "clSTT";
+            // 
+            // clMa
+            // 
+            this.clMa.DataPropertyName = "MaKS";
+            this.clMa.HeaderText = "Ma Ke Sach";
+            this.clMa.Name = "clMa";
+            // 
+            // clTen
+            // 
+            this.clTen.DataPropertyName = "TenKS";
+            this.clTen.HeaderText = "Ten Ke Sach";
+            this.clTen.Name = "clTen";
+            this.clTen.Width = 180;
+            // 
+            // clVT
+            // 
+            this.clVT.DataPropertyName = "ViTri";
+            this.clVT.HeaderText = "Vi Tri";
+            this.clVT.Name = "clVT";
+            // 
+            // buttonThem
+            // 
+            this.buttonThem.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThem.Location = new System.Drawing.Point(12, 192);
+            this.buttonThem.Name = "buttonThem";
+            this.buttonThem.Size = new System.Drawing.Size(99, 42);
+            this.buttonThem.TabIndex = 20;
+            this.buttonThem.Text = "Thêm";
+            this.buttonThem.UseVisualStyleBackColor = true;
+            this.buttonThem.Click += new System.EventHandler(this.buttonThem_Click);
+            // 
+            // buttonADD
+            // 
+            this.buttonADD.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonADD.Location = new System.Drawing.Point(145, 192);
+            this.buttonADD.Name = "buttonADD";
+            this.buttonADD.Size = new System.Drawing.Size(98, 42);
+            this.buttonADD.TabIndex = 21;
+            this.buttonADD.Text = "Lưu";
+            this.buttonADD.UseVisualStyleBackColor = true;
+            this.buttonADD.Click += new System.EventHandler(this.buttonADD_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdate.Location = new System.Drawing.Point(289, 192);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(96, 42);
+            this.buttonUpdate.TabIndex = 22;
+            this.buttonUpdate.Text = "Sửa";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonXoa
+            // 
+            this.buttonXoa.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonXoa.Location = new System.Drawing.Point(407, 192);
+            this.buttonXoa.Name = "buttonXoa";
+            this.buttonXoa.Size = new System.Drawing.Size(89, 42);
+            this.buttonXoa.TabIndex = 23;
+            this.buttonXoa.Text = "Xóa";
+            this.buttonXoa.UseVisualStyleBackColor = true;
+            this.buttonXoa.Click += new System.EventHandler(this.buttonXoa_Click);
+            // 
+            // Ten
+            // 
+            this.Ten.AutoSize = true;
+            this.Ten.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ten.Location = new System.Drawing.Point(12, 9);
+            this.Ten.Name = "Ten";
+            this.Ten.Size = new System.Drawing.Size(134, 40);
+            this.Ten.TabIndex = 24;
+            this.Ten.Text = "Kệ Sách";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 19);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Mã Kệ Sách";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 19);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Tên Kệ Sách";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 19);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Vị Trí";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 19);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Tìm theo tên kệ sách:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TimKiem
+            // 
+            this.TimKiem.Location = new System.Drawing.Point(158, 148);
+            this.TimKiem.Name = "TimKiem";
+            this.TimKiem.Size = new System.Drawing.Size(124, 20);
+            this.TimKiem.TabIndex = 34;
+            this.TimKiem.TextChanged += new System.EventHandler(this.TimKiem_TextChanged);
+            // 
+            // ViTri
+            // 
+            this.ViTri.Location = new System.Drawing.Point(158, 119);
+            this.ViTri.Name = "ViTri";
+            this.ViTri.Size = new System.Drawing.Size(124, 20);
+            this.ViTri.TabIndex = 35;
+            // 
+            // TenKS
+            // 
+            this.TenKS.Location = new System.Drawing.Point(158, 88);
+            this.TenKS.Name = "TenKS";
+            this.TenKS.Size = new System.Drawing.Size(124, 20);
+            this.TenKS.TabIndex = 36;
+            // 
+            // MaKS
+            // 
+            this.MaKS.Location = new System.Drawing.Point(158, 60);
+            this.MaKS.Name = "MaKS";
+            this.MaKS.Size = new System.Drawing.Size(124, 20);
+            this.MaKS.TabIndex = 37;
+            // 
+            // KeSach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 409);
+            this.Controls.Add(this.MaKS);
+            this.Controls.Add(this.TenKS);
+            this.Controls.Add(this.ViTri);
+            this.Controls.Add(this.TimKiem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ten);
+            this.Controls.Add(this.buttonXoa);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonADD);
+            this.Controls.Add(this.buttonThem);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "KeSach";
+            this.Text = "Thông tin kệ sách";
+            this.Load += new System.EventHandler(this.KeSach_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonThem;
+        private System.Windows.Forms.Button buttonADD;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonXoa;
+        private System.Windows.Forms.Label Ten;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TimKiem;
+        private System.Windows.Forms.TextBox ViTri;
+        private System.Windows.Forms.TextBox TenKS;
+        private System.Windows.Forms.TextBox MaKS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clSTT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clMa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clVT;
+    }
+}
